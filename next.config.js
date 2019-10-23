700e62fbc272a9b81dd2a2b1d97103de73212e27
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-10-23 16:18:39 -0500

    feat: add workbox config in next

--- next.config.js
+++ next.config.js
@@ -1,3 +1,40 @@
 const withCSS = require('@zeit/next-css')
+const withOffline = require('next-offline')
 
-module.exports = withCSS({})
+module.exports = withCSS(
+  withOffline({
+    workboxOpts: {
+      swDest: 'static/service-worker.js',
+      runtimeCaching: [
+        {
+          urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif)/,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'image-cache',
+            cacheableResponse: {
+              statuses: [0, 200]
+            },
+            expiration: {
+              maxEntries: 100,
+              maxAgeSeconds: 60 * 60 * 24 // 1 day
+            }
+          }
+        },
+        {
+          urlPattern: new RegExp('^https://api.github.com'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'api-cache',
+            cacheableResponse: {
+              statuses: [0, 200]
+            },
+            expiration: {
+              maxEntries: 100,
+              maxAgeSeconds: 60 * 60 * 24 // 1 day
+            }
+          }
+        }
+      ]
+    }
+  })
+)
