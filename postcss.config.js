86722db656a5ca75f73abd8267a6a71f82b73c90
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-10-23 13:03:08 -0500

    perf: add purge css to postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,13 @@
 /* eslint global-require: "off" */
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.tsx'],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
 
 module.exports = {
   plugins: [
     require('tailwindcss')('./tailwind.config.js'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
   ]
 }
